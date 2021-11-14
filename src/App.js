3a213e61a3cdc7bcfa82bb36a39fa9d1ed2a7b7e
Yusun <solarpy@hanyang.ac.kr>
2021-11-14 23:31:42 +0900

    Add click event
    - onClick 이벤트 생성

--- src/App.js
+++ src/App.js
@@ -2,28 +2,47 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-
-  let [title, setTitle] = useState(['남자 코트 추천', '여자 코트 추천', '숙제']); 
+  let [title, setTitle] = useState([
+    "남자 코트 추천",
+    "여자 코트 추천",
+    "숙제",
+  ]);
+  let [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div className='list'>
-        <h3>{ title[0] }</h3>
+      <button onClick={( { target : { title} }) => { setTitle([
+    "남자 가디건 추천",
+    "여자 코트 추천",
+    "숙제",
+  ]) }}>버튼</button>
+      <div className="list">
+        <h3>
+          {title[0]}{" "}
+          <span
+            onClick={() => {
+              setCount(count++);
+            }}
+          >
+            ✨
+          </span>{" "}
+          {count}
+        </h3>
         <p>2021-11-14</p>
-        <hr/>
-      </div>      
-      <div className='list'>
-        <h3>{ title[1] }</h3>
-        <p>2021-11-14</p>
-        <hr/>
+        <hr />
       </div>
-      <div className='list'>
-        <h3>{ title[2] }</h3>
+      <div className="list">
+        <h3>{title[1]}</h3>
         <p>2021-11-14</p>
-        <hr/>
+        <hr />
+      </div>
+      <div className="list">
+        <h3>{title[2]}</h3>
+        <p>2021-11-14</p>
+        <hr />
       </div>
     </div>
   );
